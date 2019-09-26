95688a2d51b84e47eaaa0791a0e80849c93ecae4
Jerome Baude <jerome@hashicorp.com>
2019-09-26 15:09:37 +0200

    add fargate module

--- main.tf
+++ main.tf
@@ -7,6 +7,38 @@ provider "aws" {
   region  = "${var.region}"
 }
 
+module "networking" {
+  source                                      = "jnonino/networking/aws"
+  version                                     = "2.0.3"
+  name_preffix                                = "base"
+  profile                                     = "aws_profile"
+  region                                      = "eu-north-1"
+  vpc_cidr_block                              = "192.168.0.0/16"
+  availability_zones                          = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  public_subnets_cidrs_per_availability_zone  = ["192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19"]
+  private_subnets_cidrs_per_availability_zone = ["192.168.128.0/19", "192.168.160.0/19", "192.168.192.0/19", "192.168.224.0/19"]
+}
+
+module "ecs-fargate" {
+  source                       = "app.terraform.io/jerome-playground-2019/ecs-fargate/aws"
+  version                      = "2.0.4"
+  name_preffix                 = "${var.prefix}"
+  profile                      = "aws_profile"
+  region                       = "${var.region}"
+  vpc_id                       = "${module.networking.vpc_id}"
+  availability_zones           = "${module.networking.availability_zones}"
+  public_subnets_ids           = "${module.networking.public_subnets_ids}"
+  private_subnets_ids          = "${module.networking.private_subnets_ids}"
+  container_name               = "${var.prefix}"
+  container_image              = "scarolan/palacearcade:latest"
+  essential                    = true
+  container_port               = 80
+  environment                  = []
+}
+output "lb_dns_name" {
+    value = "${module.ecs-fargate.lb_dns_name}"
+}
+
 #resource "tls_private_key" "hashicat" {
 #  algorithm = "RSA"
 #}
