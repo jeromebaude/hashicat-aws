b0b6afb51a6793bebecd95a2ca244550476338a3
Jerome Baude <jerome@hashicorp.com>
2019-09-30 22:28:44 +0200

    initial restore

--- clean.sh
+++ clean.sh
@@ -5,9 +5,9 @@ rm -f remote_backend.tf
 cp ./ORG/deploy_app.sh ./files/deploy_app.sh
 cp ./ORG/main.tf ./
 cp ./ORG/outputs.tf ./
-cp ./ORG/terraform.tfvars terraform.tfvars.example
+cp ./ORG/terraform.tfvars terraform.tfvars
 
-git add remote_backend.tf ./files/deploy_app.sh main.tf outputs.tf terraform.tfvars.example clean.sh
+git add *
 git commit -m "initial restore"
 git push origin master
 
