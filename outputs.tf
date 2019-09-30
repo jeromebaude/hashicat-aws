734d05e2ad5a2a771d007ced647df9ac6b8ee19d
Jerome Baude <jerome@hashicorp.com>
2019-09-30 11:19:58 +0200

    demo using modules

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 # Outputs file
-output "catapp_url" {
-  value = "http://${aws_eip.hashicat.public_ip}"
-}
+#output "catapp_url" {
+#  value = "http://${aws_eip.hashicat.public_ip}"
+#}
 
 #output "private_key" {
 #  value = "${tls_private_key.hashicat.private_key_pem}"
