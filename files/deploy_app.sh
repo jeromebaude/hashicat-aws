0a803bd9523631dba0a33eb33be76eed1c8bfda1
Jérôme Baude <jerome.baude@gmail.com>
2019-09-30 22:07:32 +0200

    Update welcoming msg

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Hello Sanofi. How are you doing from DevTestBranch?. 
+  Welcome to ${PREFIX}'s app. Hello Sanofi. How are you doing from DevTestBranch??. 
   <!-- END -->
   
   </div>
