caa7c45079f21bb807fd783ca78b1f8228191c39
Jerome Baude <jerome@hashicorp.com>
2019-10-31 10:16:51 +0100

    initial restore

--- variables.tf
+++ variables.tf
@@ -14,6 +14,10 @@ variable "region" {
   default     = "eu-north-1"
 }
 
+variable "ami" {
+  description = "The AMI id being used"
+}
+
 variable "address_space" {
   description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
   default     = "10.0.0.0/16"
