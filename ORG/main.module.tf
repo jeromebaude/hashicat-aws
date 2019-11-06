6f0bbc9ca0b4c1b376320145fd6ad7347d8746db
Jerome Baude <jerome@hashicorp.com>
2019-11-06 16:28:44 +0100

    update networking module

--- ORG/main.module.tf
+++ ORG/main.module.tf
@@ -7,16 +7,16 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-module "networking" {
-  source                                      = "jnonino/networking/aws"
-  version                                     = "2.0.3"
-  name_preffix                                = "base"
-  profile                                     = "aws_profile"
-  region                                      = "us-west-2"
-  vpc_cidr_block                              = "192.168.0.0/16"
-  availability_zones                          = ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-2d"]
-  public_subnets_cidrs_per_availability_zone  = ["192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19"]
-  private_subnets_cidrs_per_availability_zone = ["192.168.128.0/19", "192.168.160.0/19", "192.168.192.0/19", "192.168.224.0/19"]
+module "networking" { 
+        source                                      = cn-terraform/networking/aws
+        version                                     = 2.0.3
+        name_preffix                                = base
+        profile                                     = aws_profile
+        region                                      = us-east-1
+        vpc_cidr_block                              = 192.168.0.0/16
+        availability_zones                          = [ us-east-1a, us-east-1b, us-east-1c, us-east-1d ]
+        public_subnets_cidrs_per_availability_zone  = [ 192.168.0.0/19, 192.168.32.0/19, 192.168.64.0/19, 192.168.96.0/19 ]
+        private_subnets_cidrs_per_availability_zone = [ 192.168.128.0/19, 192.168.160.0/19, 192.168.192.0/19, 192.168.224.0/19 ]
 }
 
 module "ecs-fargate" {
